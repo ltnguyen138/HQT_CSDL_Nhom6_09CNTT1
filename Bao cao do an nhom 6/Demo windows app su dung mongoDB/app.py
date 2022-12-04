c0d6b5d1d6a3357d2240820286fd7221002392c9
ltnguyen138 <ltnguyen1382@gmail.com>
2022-12-04 22:22:06 +0700

    Create app.py

---
+++ Bao cao do an nhom 6/Demo windows app su dung mongoDB/app.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+
+class App:
+    def __init__(self,window):
+        window.geometry("1600x700+200+200")
+        window.title("Restaurans")
+        window["background"]="#F5F5F5"
+        #ngan chinh kich thuoc cua so
+        # window.resizable(False,False)
+    
